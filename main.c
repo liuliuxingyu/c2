33e479c4ec0994b75ce146c2bf1b16d2f650b625
邵楠 <1985890648@qq.com>
2019-01-15 20:02:44 +0800

    敏感字符替换

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 char arr[1024];
 int index=0;
-
+int isMingan(char x){
+    int count=0;
+    for(int i=0;i<index;i++){
+        if(x==arr[i]){
+            count++;
+        }
+    }
+    if(count==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 
 int main(){
     printf("minganzifuguanlixitong\n");
@@ -45,10 +57,29 @@ int main(){
             scanf("%c",&x);
         }
         if(code==4){
+            printf("qingshuruyiduanyingwen:\n");
+            char neirong[1024];
+            scanf("%s",neirong);
+            for(int i=0;1;i++){
+                if(neirong[i]=='\0'){
+                    break;
+                }
+                if( isMingan(arr[i])==1){
+                    neirong[i]='*';
+                }
+                printf("%c",neirong[i]);
+            }
+            printf("\n");
+            printf("tihuanwancheng,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
 
         }
         if(code==5){
-            
+            printf("tuichuchengxu\n");
+            break;
+
         }
     }
 
