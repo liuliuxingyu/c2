f42cc2a2cac6e2aae5d9c7d8afb01410c0c8ed27
邵楠 <1985890648@qq.com>
2019-01-15 19:38:24 +0800

    添加删除查看敏感字符

--- main.c
+++ main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
+char arr[1024];
+int index=0;
+
 
 int main(){
-    printf("hello word\n");
+    printf("minganzifuguanlixitong\n");
+    while(1){
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu(zuihouyige)\n");
+        printf("3---chakansuoyouminganzifu\n");
+        printf("4---tihuanneironfzhongdeminganzifu\n");
+        printf("5---tuichu\n");
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1){
+            printf("qingshuruyaotianjiademinganzifu\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index]=m;
+            index++;
+            printf("tianjiaxhenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x); 
+        }
+        if(code==2){
+            printf("shancuyigeminganzifu\n");
+            index--;
+            printf("shanchuchenggong,dianjihuichejixu\n");
+             char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if(code==3){
+            printf("suoyouminganzifuruxia:\n");
+            for(int i=0;i<index;i++){
+                printf("di%dgeminganzifuwei:%c\n",i+1,arr[i]);
+            }
+            printf("shanchuchenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if(code==4){
+
+        }
+        if(code==5){
+            
+        }
+    }
 
     return 0;
 
-}
+}                                                                                                       
